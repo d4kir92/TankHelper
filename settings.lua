d9849224d0b607387fb50771123d211472c70877
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-04-09 22:15:40 +0200

    v1.9.45

--- settings.lua
+++ settings.lua
@@ -310,7 +310,7 @@ function frame:OnEvent(event)
 			THTAB["MMBTN"] = TankHelper:GetWoWBuild() ~= "RETAIL"
 		end
 
-		TankHelper:SetVersion(132362, "1.9.44")
+		TankHelper:SetVersion(132362, "1.9.45")
 		TankHelper:InitSettings()
 		TankHelper:InitSetup()
 	end
