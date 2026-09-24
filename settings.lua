7131f5a6acab6f8dbe53f7b188fe822b40c436f0
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-09-24 16:53:50 +0200

    v1.10.6

--- settings.lua
+++ settings.lua
@@ -296,7 +296,7 @@ function frame:OnEvent(event)
 		THTAB = THTAB or {}
 		THTAB["MMBTNTAB"] = THTAB["MMBTNTAB"] or {}
 		if THTAB["MMBTN"] == nil then THTAB["MMBTN"] = TankHelper:GetWoWBuild() ~= "RETAIL" end
-		TankHelper:SetVersion(132362, "1.10.5")
+		TankHelper:SetVersion(132362, "1.10.6")
 		TankHelper:InitSettings()
 		TankHelper:InitSetup()
 	end
